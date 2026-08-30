bc6236202a2845048021d1e241fb3533982e4f94
Matthew Stanley <1379tech@gmail.com>
2026-08-30 12:36:18 -0700

    Use SDL2 backend for AppImage packaging

--- tools/package_appimage.sh
+++ tools/package_appimage.sh
@@ -227,6 +227,7 @@ if [ "$skip_build" = "0" ]; then
     # otherwise identical builds differ.
     cmake -S "$root" -B "$build_dir" -G "$generator" \
         -DCMAKE_BUILD_TYPE=Release \
+        -DPSX_SDL_BACKEND=SDL2 \
         -DPSX_DEBUG_TOOLS=OFF \
         -DCMAKE_EXE_LINKER_FLAGS="-Wl,--build-id=none"
     cmake --build "$build_dir" --target "$runtime_target" -j "$jobs"
